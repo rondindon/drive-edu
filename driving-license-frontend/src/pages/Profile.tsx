b9a4b18c3136dd7ad55045f36d24f3838b7f21d4
rondindon <ronnie.rybarik@gmail.com>
2025-02-14 18:00:44 +0100

    pfp update finak

--- driving-license-frontend/src/pages/Profile.tsx
+++ driving-license-frontend/src/pages/Profile.tsx
@@ -96,7 +96,7 @@ const Profile: React.FC = () => {
   // Handler for selecting a new profile picture
   const handleProfilePicSelect = async (url: string) => {
     setProfilePicture(url);
-    await axios.put('https://drive-edu.onrender.com/api/users/updateProfilePic', { profilePicture: url }, { headers: { Authorization: `Bearer ${token}` } });
+    axios.put('https://drive-edu.onrender.com/api/users/updateProfilePicture', { profilePicture: url }, { headers: { Authorization: `Bearer ${token}` } });
     setProfilePicDialogOpen(false);
   };
 
